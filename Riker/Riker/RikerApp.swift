742a909f3b878d33d268a739c812935d70e0d34d
Paul Evans <evansp2@gmail.com>
2023-01-11 07:27:26 -0500

    swiftui initial commit

---
+++ Riker/Riker/RikerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RikerApp.swift
+//  Riker
+//
+//  Created by Paul Evans on 12/16/22.
+//
+
+import SwiftUI
+
+@main
+struct RikerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
